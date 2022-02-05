f7d5eec7749b9194692aabd3e7a5617d28c787c1
Sunila Mahapatra <sunil.mahapatra@in.ibm.com>
2022-02-05 20:36:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src="https://media.giphy.com/media/yoJC2GnSClbPOkV0eA/giphy.gif" alt="logo" />
+        </header>
     </div>
   );
 }
